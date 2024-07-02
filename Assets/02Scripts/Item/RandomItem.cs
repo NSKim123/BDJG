39ebc14758fe7308b9d0ab63c130cd5ba3c05c53
NSKim123 <kimnamseok15@gmail.com>
2024-07-03 04:08:48 +0900

    Work Item Animation

--- Assets/02Scripts/Item/RandomItem.cs
+++ Assets/02Scripts/Item/RandomItem.cs
@@ -7,6 +7,19 @@ public class RandomItem : MonoBehaviour
     [SerializeField]
     private ItemProbabilityTableScriptableObject _ItemProbabilityTableScriptableObject;
 
+    private float _InstantiatedTime;
+
+    private void Awake()
+    {
+        Destroy(this.gameObject, 20.0f);
+        _InstantiatedTime = Time.time;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.up * 0.01f * Mathf.Pow(Time.time - _InstantiatedTime, 2.0f) * Time.fixedDeltaTime;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Player"))
